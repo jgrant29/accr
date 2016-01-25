f1c64d64108ffeef714c3d18a714747c32a6dc0b
Justin Grant <jmgrant702@gmail.com>
2016-01-24 18:39:15 -0700

    first draft

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( accr2_logo.png )
+Rails.application.config.assets.precompile += %w( human_resources.jpg )
